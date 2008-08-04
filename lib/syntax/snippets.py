728955310c7b293d99059255c9e0e3a94b45dbcd
Mark Chadwick <mchadwick@mchadwick-desktop.(none)>
2008-08-03 21:41:05 -0400

    Stubbing out language picker

--- lib/syntax/snippets.py
+++ lib/syntax/snippets.py
@@ -1,8 +1,10 @@
 SNIPPETS = {
-    #
-    # Python
-    #
-    'python':   """#!/usr/bin/env python
+# --------------------------------------------------------------------------
+# Python
+# --------------------------------------------------------------------------
+'python':
+
+"""#!/usr/bin/env python
 
 import os
 import sys
@@ -24,13 +26,69 @@ if __name__ == '__main__':
 
 """,
 
-    #
-    # C
-    #
-    'c':        """include <stdio.h>
 
-int main(int argc, char *argv) {
-    printf("Hello world!\n");
-}""",
+# ------------------------------------------------------------------------------
+# Ruby
+# ------------------------------------------------------------------------------
+"ruby":
+
+"""#!/usr/bin/ruby
+# Syntax Highlighting Test File for Ruby
+# Some Comments about this file
+# Hello World in ruby
+
+# Keyword statement and string
+puts 'Hello world'
+
+# Function Definitions
+def hello2(name)
+    puts "Hello #{name}!"
+end
+
+# Class Definition
+class Greeter
+    def intialize(name = "World")
+        @name = name
+    end
+    def say_hello
+        puts "Hello #{@name}!"
+    end
+    def say_bye
+        puts "Bye #{@name}, come again."
+    end
+end
+
+# Keyword and some Numbers
+puts 5 ** 2
+""",
+
+# ------------------------------------------------------------------------------
+# C
+# ------------------------------------------------------------------------------
+'c':
+
+"""/** Syntax Highlighting Test File for C
+ *  \\brief Doxygen tag highlighting
+ */
+
+#include"stdlib.h"
+
+int main()
+{
+    int val1 = 10;
+    int val2 = val1 * 2;
+    char mychar = 'A';
+
+    for(;;) {
+        val2 = val1 - 1;
+        if(val <= 0) break;
+    }
+
+	printf("Hello World, %d + %d = %d", val1, val2, val1 + val2);
+	printf("An unclosed string and unmatched brace
+}/*End Main*/
+
+/*EOF*/
+""",
 
 }
